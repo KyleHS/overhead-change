3d3d196f5ee1ca19282ec973247aaeb6f3df036e
KyleHS <k15156@yahoo.com>
2024-06-12 13:49:35 -0600

    Added in template instructions within plugin file for barrows sounds

---
+++ src/main/java/com/overhead/OverheadPlugin.java
@@ -0,0 +1,61 @@
+package com.overhead;
+
+import com.google.inject.Provides;
+import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.ChatMessageType;
+import net.runelite.api.Client;
+import net.runelite.api.GameState;
+import net.runelite.api.events.GameStateChanged;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+
+@Slf4j
+@PluginDescriptor(
+	name = "Example"
+)
+public class OverheadPlugin extends Plugin
+{
+	@Inject
+	private OverlayManager overlayManager;
+
+	@Inject
+	private BarrowsChestConfig config;
+
+	@Provides
+	BarrowsChestConfig provideConfig(ConfigManager configManager) {
+		return configManager.getConfig(BarrowsChestConfig.class);
+	}
+
+	@Subscribe
+	public void onChatMessage(ChatMessage event) {
+		// Check for the specific chat message indicating a Barrows chest opening
+		if (event.getMessage().contains("You've found a hidden tunnel!") || event.getMessage().contains("You find a piece of the Barrows equipment")) {
+			// Play sound
+			playSound();
+		}
+	}
+
+	private void playSound() {
+		try {
+			File soundFile = new File("path/to/your/soundfile.wav");
+			Clip clip = AudioSystem.getClip();
+			clip.open(AudioSystem.getAudioInputStream(soundFile));
+			clip.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
+
+	@Provides
+    OverheadConfig provideConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(OverheadConfig.class);
+	}
+}
--- src/test/java/com/overhead/OverheadPluginTest.java
+++ src/test/java/com/overhead/OverheadPluginTest.java
@@ -1,13 +1,14 @@
-package com.example;
+package com.overhead;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class OverheadPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(OverheadPlugin.class);
 		RuneLite.main(args);
 	}
+
 }
