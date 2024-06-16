a675d1f3529ce802e76805569a4b35e490da407b
KyleHS <k15156@yahoo.com>
2024-06-16 12:36:51 -0600

    Worked through bugs and updated method to contain accurate barrows item IDs along with the opened barrows chest

--- src/main/java/com/overhead/OverheadPlugin.java
+++ src/main/java/com/overhead/OverheadPlugin.java
@@ -3,18 +3,17 @@ package com.overhead;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.events.WidgetLoaded;
 import net.runelite.api.Client;
+import net.runelite.api.InventoryID;
 import net.runelite.api.Item;
+import net.runelite.api.MenuAction;
+import net.runelite.api.events.MenuEntryAdded;
+import net.runelite.api.events.MenuOptionClicked;
 import net.runelite.client.config.ConfigManager;
-import net.runelite.client.events.ConfigChanged;
 import net.runelite.client.eventbus.Subscribe;
-import net.runelite.client.game.ItemManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
-import net.runelite.client.ui.overlay.OverlayManager;
-import net.runelite.api.widgets.Widget;
-import net.runelite.api.widgets.WidgetInfo;
+import net.runelite.client.game.ItemManager;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -23,9 +22,9 @@ import java.io.File;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Barrows Feedback Sounds",
-	description = "Plays one of two sounds depending if a barrows unique is received",
-	tags = {"barrows", "unique", "sound", "spongebob", "magic conch", "sound adder"}
+		name = "Barrows Feedback Sounds",
+		description = "Plays one of two sounds depending if a barrows unique is received",
+		tags = {"barrows", "unique", "sound", "spongebob", "magic conch", "sound adder"}
 )
 public class OverheadPlugin extends Plugin
 {
@@ -38,50 +37,82 @@ public class OverheadPlugin extends Plugin
 	@Inject
 	private ItemManager itemManager;
 
+	private static final int BARROWS_CHEST_ID = 20724; // Opened Barrows Chest ID
+
 	@Provides
 	BarrowsChestConfig provideConfig(ConfigManager configManager) {
 		return configManager.getConfig(BarrowsChestConfig.class);
 	}
 
 	@Subscribe
-	public void onWidgetLoaded(WidgetLoaded event)
+	public void onMenuEntryAdded(MenuEntryAdded event)
 	{
-		if (event.getGroupId() == WidgetInfo.BARROWS_REWARD_GROUP_ID)
+		if (event.getOption().equalsIgnoreCase("open") && event.getIdentifier() == BARROWS_CHEST_ID)
 		{
-			checkBarrowsChestContents();
+			client.createMenuEntry(-1)
+					.setOption("Check")
+					.setTarget(event.getTarget())
+					.setType(MenuAction.RUNELITE)
+					.onClick(e -> checkBarrowsChestContents());
 		}
 	}
 
 	private void checkBarrowsChestContents()
 	{
-		Widget rewardsWidget = client.getWidget(WidgetInfo.BARROWS_REWARD_INVENTORY);
-		if (rewardsWidget != null)
+		Item[] items = client.getItemContainer(InventoryID.BARROWS_REWARD).getItems();
+
+		boolean hasBarrowsItem = false;
+
+		for (Item item : items)
 		{
-			Item[] items = rewardsWidget.getWidgetItems();
-
-			boolean hasBarrowsItem = false;
-
-			for (Item item : items)
+			if (isBarrowsItem(item.getId()))
 			{
-				if (isBarrowsItem(item.getId()))
-				{
-					hasBarrowsItem = true;
-					break;
-				}
+				hasBarrowsItem = true;
+				break;
 			}
-
-			playSound(hasBarrowsItem);
 		}
+
+		playSound(hasBarrowsItem);
 	}
 
 	private boolean isBarrowsItem(int itemId)
 	{
-		// List of Barrows item IDs (replace with actual IDs)
 		int[] barrowsItemIds = {
-				// Example IDs, replace with actual Barrows item IDs
+				//ahrim's
 				4708, // Ahrim's hood
 				4710, // Ahrim's staff
-				// Add all Barrows item IDs here
+				4712, // Ahrim's robetop
+				4714, // Ahrim's robeskirt
+
+				//dharok's
+				4716, // Dharok's helm
+				4720, // Dharok's platebody
+				4722, // Dharok's platelegs
+				4718, // Dharok's greataxe
+
+				//guthan's
+				4724, // Guthan's helm
+				4728, // Guthan's body
+				4730, // Guthan's chainskirt
+				4726, // Guthan's warspear
+
+				//karil's
+				4732, // Karil's coif
+				4736, // Karil's leathertop
+				4738, // Karil's leatherskirt
+				4734, // Karil's crossbow
+
+				//torag's
+				4745, // Torag's helm
+				4749, // Torag's platebody
+				4751, // Torag's platelegs
+				4747, // Torag's hammers
+
+				//verac's
+				4753, // Verac's helm
+				4757, // Verac's brassard
+				4759, // Verac's plateskirt
+				4755, // Verac's flail
 		};
 
 		for (int id : barrowsItemIds)
@@ -115,5 +146,4 @@ public class OverheadPlugin extends Plugin
 			e.printStackTrace();
 		}
 	}
-
 }
